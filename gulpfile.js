3693c67cd57664c44c43c44e71e0c491ebd43e52
Philipp Alferov <philipp.alferov@gmail.com>
2015-07-29 23:29:19 -0400

    Add deploy gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 'use strict';
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var ghPages = require('gulp-gh-pages');
 
 var path = {
   src: 'src'
@@ -13,6 +14,11 @@ gulp.task('build', function() {
     .pipe($.uglify())
     .pipe($.rename({ suffix: '.min'}))
     .pipe(gulp.dest(path.src));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./docs/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('default', ['build']);
