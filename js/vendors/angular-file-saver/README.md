308de0bc5d772c774e24dcd03ca55b949d33180a
Philipp Alferov <palferov@securesense.ca>
2015-07-29 23:29:35 -0400

    Update 2015-07-30T03:29:33.118Z

--- js/vendors/angular-file-saver/README.md
+++ js/vendors/angular-file-saver/README.md
@@ -1,6 +1,6 @@
 angular-file-saver
 =========
-Angular file saver is an AngularJS service that provides integration of [FileSaver.js](https://github.com/eligrey/FileSaver.js/) that implements the
+Angular file saver is an AngularJS service that provides integration between  [FileSaver.js](https://github.com/eligrey/FileSaver.js/) and AngularJS. FileSaver.js implements the
 HTML5 W3C saveAs() FileSaver interface in browsers that do not natively support
 it. The package also comes with [Blob.js](https://github.com/eligrey/Blob.js/)
 as a dependency to provide support of wide range of browsers.
--- js/vendors/angular-file-saver/src/file-saver.js
+++ js/vendors/angular-file-saver/src/file-saver.js
@@ -13,41 +13,20 @@
 
   angular
     .module('fileSaver', [])
-    .factory('SaveAs', [SaveAs]);
+    .factory('SaveAs', SaveAs);
 
-    function SaveAs () {
-      function blobInit(data, type) {
-        var blob;
+    function SaveAs() {
 
-        if (hasBlobSupport()) {
-          return new Blob(data, type);
-        }
-
-        blob = new BlobBuilder();
-        blob.append(data[0]);
-        blob = getBlob(type.type);
-
-        return blob;
+      function isBlobInstance (data) {
+        return data instanceof Blob;
       }
 
-      function isBlobInstance(data) {
-        var blobSupport = hasBlobSupport();
-
-        if (blobSupport && data instanceof Blob) {
-          return true;
+      function save(blob, filename) {
+        try {
+          saveAs(blob, filename);
+        } catch(err) {
+          console.error(err.message);
         }
-
-        //If browser does not suport Blob natively, but the object passed as
-        //first argument represented as BlobBuilder polyfill instance
-        if (!blobSupport && data instanceof BlobBuilder) {
-          return true;
-        }
-
-        return false;
-      }
-
-      function hasBlobSupport () {
-        return typeof(Blob) === "function";
       }
 
       return {
@@ -55,29 +34,24 @@
         /**
         * saveFile - Immediately starts saving a file, returns undefined.
         *
-        * @param  {string|array|object} data Data, represented as a string,
-        * an array or a Blob object;
+        * @param  {array|Blob} data Represented as an array or a Blob object
         * @param  {string} filename
-        * @param  {object} options Set of options for the Blob constructor.
+        * @param  {object} options Set of Blob constructor options.
         * Optional parameter, if Blob object is passed as first argument
         * @return {undefined}
         */
 
-        saveFile: function (data, filename, options) {
+        download: function (data, filename, options) {
           var blob;
-          options = (typeof options === 'undefined') ? {} : options;
-
-          if (isBlobInstance(data)) {
-            return saveAs(data, filename);
-          }
-
           data = data instanceof Array ? data : [data];
 
-          blob = blobInit(data, options);
+          if (isBlobInstance(data)) {
+            save(data, filename);
+          }
 
-          return saveAs(blob, filename);
+          blob = new Blob(data, options);
+          save(blob, filename);
         }
-
       };
     }
 
