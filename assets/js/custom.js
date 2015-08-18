ddbba47b2982fe9e1333e9dfbf834abafb5da896
Philipp Alferov <palferov@securesense.ca>
2015-08-18 00:26:37 -0400

    Update 2015-08-18T04:26:35.747Z

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+var angular = require('angular');
+var fileSaver = require('../../../src/angular-file-saver');
+window.saveAs = require('browser-filesaver');
+
+angular
+  .module('ngFileSaver', ['fileSaver'])
+  .controller('DownloadText', ['$scope', 'SaveAs', DownloadText]);
+
+function DownloadText($scope, SaveAs) {
+  var vm = this;
+
+  vm.val = {
+    text: 'Hey ho lets go!'
+  };
+
+  vm.download = function(text) {
+
+    var options =  {
+      type: "text/plain;charset=utf-8"
+    };
+
+    SaveAs.download([text], 'textfile.txt', options);
+
+  };
+}
