1afa8a11e32c1c0fbc1cf0d805fb55a96d61143f
Philipp Alferov <philipp.alferov@gmail.com>
2015-09-08 22:28:04 -0400

    Add basic test cases

--- test/angular-file-saver.spec.js
+++ test/angular-file-saver.spec.js
@@ -1,9 +1,53 @@
 'use strict';
 
-describe('Angular', function() {
+describe('angular-file-saver', function() {
 
-  it('should exist', function() {
-    expect(angular).toBeDefined();
+  var SaveAs, $window, Blob, options, filename;
+  beforeEach(module('fileSaver'));
+
+  beforeEach(inject(function(_SaveAs_, _$window_) {
+    SaveAs = _SaveAs_;
+    $window = _$window_;
+    Blob = $window.Blob;
+  }));
+
+  it('should not throw an error if dependencies are defined', function() {
+    expect($window.saveAs).toBeDefined();
+    expect($window.Blob).toBeDefined();
   });
 
+  describe('passed arguments are correct', function() {
+
+    beforeEach(function() {
+      options = { type: 'text/plain;charset=utf-8' };
+      filename = 'xfiles.txt';
+    });
+
+    it('should save a file if provided data is an array', function() {
+      var data = ['text'];
+
+      expect(function() {
+        SaveAs.download(data, filename, options);
+      }).not.toThrow();
+    });
+
+    it('should save a file if provided data is a blob', function() {
+      var data = new Blob(['text'], options);
+      console.log(data instanceof Blob);
+      expect(function() {
+        SaveAs.download(data, filename);
+      }).not.toThrow();
+    });
+
+    it('should save a file if options object is not passed', function() {
+      var data = ['text'];
+
+
+
+      expect(function() {
+        SaveAs.download(data, filename);
+      }).not.toThrow();
+    });
+
+  });
 });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
     },
     browsers: ['Chrome'],
     reporters: ['progress'],
-    autoWatch: true,
+    autoWatch: false,
     browserify: {
       debug: true,
       transform: []
@@ -18,7 +18,10 @@ module.exports = function(config) {
       '/': 'http://localhost:9876/'
     },
     urlRoot: '/__karma__/',
+    singleRun: true,
     files: [
+      './node_modules/Blob.js/Blob.js',
+      './node_modules/FileSaver.js/FileSaver.js',
       './node_modules/angular/angular.js',
       './node_modules/angular-mocks/angular-mocks.js',
       './src/angular-file-saver.js',
