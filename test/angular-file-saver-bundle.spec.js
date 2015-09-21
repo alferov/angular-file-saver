fbf3c0a567f3952b6ce05ac2f6675fae4e5a141a
Philipp Alferov <philipp.alferov@gmail.com>
2015-09-20 21:27:55 -0400

    Refine tests

--- test/angular-file-saver-bundle.spec.js
+++ test/angular-file-saver-bundle.spec.js
@@ -5,29 +5,48 @@ describe('angular-file-saver', function() {
   var FileSaver, Blob;
 
   beforeEach(function() {
-
     angular.mock.module('ngFileSaver');
-
     angular.mock.inject(function(_FileSaver_, _Blob_) {
       FileSaver = _FileSaver_;
       Blob = _Blob_;
     });
   });
 
-  describe('passed arguments are correct', function() {
+  describe('#saveAs', function() {
 
-    it('should throw an error if provided data is an array', function() {
+    it('should throw an error if `options.data` is not a blob', function() {
       var config = {
         data: ['text'],
         filename: 'xfiles.txt'
       };
-
-      expect(function() { FileSaver.saveAs(config); }).toThrow();
+      expect(function() { FileSaver.saveAs(config); })
+        .toThrowError(/should be a blob instance/);
     });
 
-    it('should save a file if provided data is a blob', function() {
-      var data = new Blob(['text'], { type: 'text/plain;charset=utf-8' });
+    it('should throw an error if `options` object is empty', function() {
+      var config = {};
+      expect(function() { FileSaver.saveAs(config); })
+        .toThrowError(/should be a blob instance/);
+    });
 
+    it('should throw an error if `options` object is not passed', function() {
+      expect(function() { FileSaver.saveAs(); })
+        .toThrowError(/should be a blob instance/);
+    });
+
+    it('should throw an error if `options.filename` is not a string', function() {
+      var data = new Blob(['text'], { type: 'text/plain;charset=utf-8' });
+      var config = {
+        data: data,
+        filename: null
+      };
+
+      expect(function() { FileSaver.saveAs(config); })
+        .toThrowError(/should be a string/);
+    });
+
+    it('should save a file if provided data is valid', function() {
+      var data = new Blob(['text'], { type: 'text/plain;charset=utf-8' });
       var config = {
         data: data,
         filename: 'xfiles.txt'
@@ -37,4 +56,5 @@ describe('angular-file-saver', function() {
     });
 
   });
+
 });
